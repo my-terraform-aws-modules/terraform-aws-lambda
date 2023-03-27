f18614d0fe5a49214d1e1da8a88894cce97b453a
archna94 <126779485+archna94@users.noreply.github.com>
2023-03-27 23:47:46 +0100

    aa

--- lambda.tf
+++ lambda.tf
@@ -20,9 +20,7 @@ resource "aws_lambda_function" "test_lambda" {
   #     TABLE_NAME = var.dynamodb_id
   #   }
   # }
-  tags = {
-    Environment = var.environment  
-  }
+  tags = var.tags
 }
 resource "aws_iam_role" "lambda" {
   count = var.create_role ? 1 : 0
