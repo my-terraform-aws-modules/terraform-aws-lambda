956567cf26c67251402e75f671833504c16c0bdf
archana <archanavadde88@gmail.com>
2023-03-27 23:40:42 +0100

    aa

---
+++ lambda.tf
@@ -0,0 +1,101 @@
+provider "aws" {
+  region = var.region 
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/python/process-sqs/process_sqs.py"
+  output_path = "${path.module}/../../python/process-sqs/process_sqs.zip"
+}
+resource "aws_lambda_function" "test_lambda" {
+  count = var.create-function ? 1 : 0
+  function_name = "${var.environment}-${var.lambda_name}"
+  filename      = data.archive_file.lambda.output_path
+  role          = var.create_role ? aws_iam_role.lambda[0].arn : var.lambda_role
+  handler       = var.lambda_handler
+  source_code_hash = filebase64sha256(data.archive_file.lambda.output_path)
+  runtime = var.runtime
+  # environment {
+  #   variables = {
+  #     TABLE_NAME = var.dynamodb_id
+  #   }
+  # }
+  tags = {
+    Environment = var.environment  
+  }
+}
+resource "aws_iam_role" "lambda" {
+  count = var.create_role ? 1 : 0
+  #function_name = aws_lambda_function.test_lambda[0].arn
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda[0].id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "sqs:*",
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Action" : [
+          "sns:*",
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Action" : [
+          "logs:*", 
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Action" : [
+          "dynamodb:*", 
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      }
+    ]
+  })
+
+}
+resource "aws_lambda_function_event_invoke_config" "example" {
+  count = var.create-event-invoke ? 1 : 0
+  function_name = aws_lambda_function.test_lambda[0].arn
+  destination_config {
+    on_failure {
+      destination = var.sns_arn
+    }
+
+    on_success {
+      destination = var.sns_arn
+    }
+    }
+}
+
---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+    type = string
+    default = "eu-west-2"  
+}
+variable "create-function" {
+    type = bool
+    default = true 
+}
+variable "environment" {
+  description = "The environment to deploy to."
+  type        = string
+  default     = "dev"
+  validation {
+    condition     = contains(["dev", "prod", "sit", "snd", "uat"], var.environment)
+    error_message = "Valid values for var: environment are (dev, prod, sit, snd, uat)."
+  }
+}
+variable "lambda_name" {
+    type = string
+    default = "demo-function" 
+}
+variable "runtime" {
+    type = string
+    default = "python3.9"  
+}
+variable "lambda_handler" {
+    description = "give filename & function name which you have mentioned in the file"
+    default = "process_sqs.lambda_handler"
+}
+variable"create_role"{
+    type = bool
+    default = true
+}
+variable "lambda_role" {
+    type = string
+    default = ""
+  
+}
+variable "create-event-invoke" {
+    type = bool
+    default = false 
+}
+variable "sns_arn" {  
+    type = string
+    default = ""
+}
+variable "dynamo_id" {
+    type = string
+    default = ""  
+}
+
+
+
+  
